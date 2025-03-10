64b1c30febacdffa58558c5cd46faf319296d186
bambandouraccel <bamba.ndour@accel-tech.net>
2025-03-10 14:56:36 +0000

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { PageNotFoundComponent } from './common/page-not-found/page-not-found.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
